079d9b0ab39b561dd0153c5764ddd3adc3016c3b
Rodrigues <Tazi.Rodrigues@dfo-mpo.gc.ca>
2025-11-10 15:33:06 -0500

    Generalized column names.

--- R/crepuscule.R
+++ R/crepuscule.R
@@ -1,27 +1,29 @@
-crepuscule <- function(df, threshold = 0, fishfilter = FALSE) {
+crepuscule <- function(df, fish_col, time_col, day_periods = "dayPeriod", threshold = 0) {
 
   ## first check - do we have sunrise and sunset times?
 
-  if(is.null(df$sunrise) == TRUE | is.null(df$sunset)) {
-    stop("When did the sun rise and set? You can use suncalc::getSunlightTimes to find out, or apply the function suntimer from the library.") }
+  if(is.null(df[[day_periods]]) == TRUE) {
+    stop("Before running this function, assign day periods. You can use salvelinr::sundial to do so.") }
 
-  if(fishfilter == TRUE) { df <- filter(df, grepl("ish", TRANSMITTER) == TRUE) }
+  ## now make sure we have dates
+
+  df$date <- as.Date(df[[time_col]])
 
   ## next check - how many detections are there for each fish each day?
 
-  tester_table <- df %>% group_by(TRANSMITTER, Date_LT, dayPeriod) %>%
+  tester_table <- df %>% group_by(df[[fish_col]], date, df[[day_periods]]) %>%
     summarise(ndet = n()) %>% filter(ndet >= threshold) ## we may want a threshold
 
   ## now we'll get the first and last detections for each night period
 
-  n1 <- df %>% filter(dayPeriod == "N1") %>% group_by(Date_LT, TRANSMITTER) %>%
-    arrange(DateTime_LT) %>%
+  n1 <- df %>% filter(df[[day_periods]] == "N1") %>% group_by(df[[time_col]], df[[fish_col]]) %>%
+    arrange(df[[time_col]]) %>%
     filter(row_number() == n()) ## gets last detections from early night
-  n2 <- df %>% filter(dayPeriod == "N2") %>% group_by(Date_LT, TRANSMITTER) %>%
-    arrange(DateTime_LT) %>%
+  n2 <- df %>% filter(df[[day_periods]] == "N2") %>% group_by(df[[time_col]], df[[fish_col]]) %>%
+    arrange(df[[time_col]]) %>%
     filter(row_number() == 1) ## gets first detections from late night
-  dayPresent <- tester_table %>% filter(dayPeriod == "Day") %>%
-    select("Date_LT", "TRANSMITTER") %>% mutate(detected_day = TRUE)
+  dayPresent <- tester_table %>% filter(df[[day_periods]] == "Day") %>%
+    select(date, fish_col) %>% mutate(detected_day = TRUE)
 
   nights <- rbind(n1, n2); nights <- merge(dayPresent, nights, all = TRUE)
 
