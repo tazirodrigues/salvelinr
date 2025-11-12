90c0494e5132a586bd69a65704dcf09c80a70909
Rodrigues <Tazi.Rodrigues@dfo-mpo.gc.ca>
2025-11-12 16:16:07 -0500

    Informally tested Nov. 12 :)

--- R/interpolateDates.R
+++ R/interpolateDates.R
@@ -1,39 +1,44 @@
 #' @title interpolateDates
 #'
-#' @description Interpolates temperature data to populate missing dates
+#' @description Interpolates temperature data to populate missing dates at the depth values for which data exist.
 #'
 #' @param df A data frame object. Currently, must contain a date column and a depth column.
 #' @param date_col Name (in quotes) of the column with dates in the data frame.
+#' @param depth_col Name (in quotes) of the column with depths
 #' @param var Variable of interest. Defaults to "Temp"
 #' @param interval Temporal resolution of interpolated data frame. Defaults to "day"
 #'
 #' @return Interpolated data for the variable of interest.
 #' @examples
-#' interpolated_temps <- interpolateDates(temp_profiles, date_col = "Date_LT", interval = "day")
+#' interpolated_temps <- interpolateDates(temp_profiles, date_col = "Date_LT", depth_col = "Depths", interval = "day")
 #' @export
 #' @importFrom zoo "na.approx"
+#' reshape2 "dcast"
 
-interpolateDates <- function(df, date_col, var = "Temp", interval = "day") {
+interpolateDates <- function(df, date_col, depth_col, var = "Temp", interval = "day") {
 
-  df$Date <- as.Date(df[[date_col]], "%Y-%m-%d")
+  all_dates <- data.frame(Date = seq.Date(min(df[[date_col]]),
+                                          max(df[[date_col]]), interval))
+  names(all_dates) <- date_col
 
-  all_dates <- data.frame(Date = seq.Date(min(df$Date),
-                                          max(df$Date), interval))
+  df <- reshape2::dcast(df, df[[date_col]] ~ df[[depth_col]], mean)
+  names(df)[names(df) == "df[[date_col]]"] <- date_col
 
-  df <- dcast(df, Date ~ Depth, mean)
-  df <- merge(all_dates, df, by = "Date", all = TRUE)
+  df <- merge(all_dates, df, by = date_col, all = TRUE)
+  df[[date_col]] <- as.numeric(as.factor(df[[date_col]]))
 
-  df$Date <- as.numeric(as.factor(df$Date))
+  df[df == "NaN"] <- NA
 
-  df <- na.approx(df)
+  df <- zoo::na.approx(df)
   df <- as.data.frame(df)
-  df <- melt(df, id.vars = c("Date"))
+  df <- reshape2::melt(df, id.vars = date_col)
 
-  names(df) <- c("number", "Depth", var)
-  df$Depth <- as.numeric(as.character(df$Depth))
+  names(df) <- c("number", depth_col, var)
+  df[[depth_col]] <- as.numeric(as.character(df[[depth_col]]))
 
-  all_dates$number <- as.numeric(as.factor(all_dates$Date))
+  all_dates$number <- as.numeric(as.factor(all_dates[[date_col]]))
   df <- merge(df, all_dates, by = "number", all = TRUE)
+  df$number <- NULL
 
   return(df)
 
