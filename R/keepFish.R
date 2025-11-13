b2b65ab43ab01c9f4d704f711d6f86828cea5f41
Tazi Rodrigues <151076758+tazirodrigues@users.noreply.github.com>
2025-11-13 16:50:54 -0500

    Fixed order of notfound and filter

--- R/keepFish.R
+++ R/keepFish.R
@@ -16,8 +16,8 @@ keepFish <- function(df, fish_col, fishnames) {
 
   if(class(df[[fish_col]]) != class(fishnames)) { stop("Both fish lists must be of the same class.") }
 
-  df <- filter(df, df[[fish_col]] %in% fishnames)
   notfound <- base::setdiff(df[[fish_col]], fishnames)
+  df <- filter(df, df[[fish_col]] %in% fishnames)
 
   if(length(notfound) > 0) {
     print(paste("The following IDs were not matched:", notfound, sep = " "))}
