d617c70bc9bfec7e234f4a9c5eec89e74dcaf1c4
Rodrigues <Tazi.Rodrigues@dfo-mpo.gc.ca>
2025-11-17 10:24:36 -0500

    Added purrr dependency.

--- R/readDetectsFolder.R
+++ R/readDetectsFolder.R
@@ -17,7 +17,7 @@ readDetectsFolder <- function(foldername) {
   filenames <- list.files(foldername, pattern = "\\.csv$", full.names = TRUE) # take all files ending in csv
   ldf <- lapply(filenames, read.csv) # apply read.csv to the whole list
 
-  df <- ldf %>% map_df(~.); rm(ldf)
+  df <- ldf %>% purrr::map_df(~.); rm(ldf)
 
   return(df)
 
---
+++ tests/testthat/test-readDetectsFolder.R
@@ -0,0 +1,11 @@
+# this test is commented out because it relies on access to my files
+# but it passes :)
+
+# test_that("read folder works", {
+#
+#   foldername <- "~/L626 Data/VR2 Data/L626 VR2 2021-22/csv files"
+#
+#   expect_equal(class(readDetectsFolder(foldername)),
+#                "data.frame")
+#
+# })
